79d9a9d3abe92758b250ab8b2289ee7492fa8db5
leopereiro <lperpom.genius@gmail.com>
2025-05-21 13:14:15 +0200

    actualización

--- server.js
+++ server.js
@@ -1,23 +1,35 @@
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
 const app = express();
 
 
+app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 // Accedo o arquivo estático
 app.use(express.static(path.join(__dirname, "static")));
 app.post("/acceso", (req, res) => {
-    console.log(req.body);
-    if(req.body.nome === "Leo" && req.body.email === "leo@leo.com") {
-    res.sendFile(path.join(__dirname, "static/views/app.html"));
+    const {nome, email} = req.body;
+    console.log(nome, email);
+    let condicionUsuarioCorrecto = req.body.nome == "Leo" && req.body.email == "leo@leo.com";
+    let datoEnviadoCondicionUsuarioCorrecto = {
+            resposta: "acceso autorizado",
+            usuario:{
+                nome: "Leo"
+            }
+        }
+    let datoEnviadoEnErro = {resposta:"faltan campos ou usuario non rexistrado"}
+    
+    if(condicionUsuarioCorrecto){
+        res.send(datoEnviadoCondicionUsuarioCorrecto);
+    
     }else{
-        res.send("<h1>Usuario non autorizado</h1>")
+        res.send(datoEnviadoEnErro);
     }
 });
-app.get("/enviousuario", (req, res) => {
-    res.send({usuario:{
-        nome: "Leo"
-    }})
+
+app.get("/app", (req,res) => {
+    res.sendFile(path.join(__dirname, "static/views/app.html"));
 })
 
 //START SERVER
